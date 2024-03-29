90355deb8a05d0c872444b4d429069145e8e2bea
Anraaa <aqlaharun03@gmail.com>
2024-03-30 06:29:02 +0700

    fresh from the oven

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("hellooo")
+from scapy.all import *
+
+def network_scan(target_ip, timeout=2):
+    # Buat paket ICMP Echo Request
+    icmp_request = IP(dst=target_ip) / ICMP()
+
+    # Kirim paket dan terima respon
+    ans, _ = sr(icmp_request, timeout=timeout, verbose=False)
+
+    # Proses hasil respon
+    live_hosts = []
+    for sent_packet, received_packet in ans:
+        live_hosts.append(received_packet[IP].src)
+
+    return live_hosts
+
+# Contoh penggunaan
+target_network = "192.168.1.0/24"
+live_hosts = network_scan(target_network)
+print("Host yang hidup di dalam jaringan:", live_hosts)
--- yolo.py
+++ yolo.py
@@ -1,1 +1,1 @@
-print("yolooo")
+print("yolo")
